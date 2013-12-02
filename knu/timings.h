9337e588ea62021064077403d60508f43a16654f
Brian Michael Jones <spamthiseml@hotmail.com>
2013-12-01 19:00:30 -0500

    Added Mpf to timings

--- knu/timings.h
+++ knu/timings.h
@@ -4,6 +4,8 @@
 // Created 8/29/2013
 
 #include <chrono>
+#include <vector>
+
 namespace knu
 {
 	class Timings
@@ -26,8 +28,44 @@ namespace knu
 		std::chrono::nanoseconds diff_nano() {
 			return t2 - t1;
 		}
-
 	};
+    
+    class Mpf
+    {
+    public:
+        
+        Mpf():
+        lastTime(std::chrono::high_resolution_clock::now()),
+        currentTime(std::chrono::high_resolution_clock::now()),
+        aSecond(1)
+        {
+            totalTime = currentTime - lastTime;
+            times.reserve(1000);
+        }
+        void tick()
+        {
+            currentTime = std::chrono::high_resolution_clock::now();
+            totalTime += (currentTime - lastTime);
+            times.push_back(currentTime - lastTime);
+            if(totalTime >= aSecond)
+            {
+                auto tt = std::accumulate(std::begin(times), std::end(times), std::chrono::duration<float, std::milli>());
+                averageMs = (float)std::chrono::duration_cast<std::chrono::milliseconds>(tt).count() / times.size();
+                times.clear();
+                totalTime = std::chrono::milliseconds(0);
+            }
+            
+            lastTime = currentTime;
+        }
+        
+        float average_ms() const { return averageMs; }
+    private:
+        std::chrono::high_resolution_clock::time_point lastTime, currentTime;
+        std::chrono::seconds aSecond;
+        std::chrono::high_resolution_clock::duration totalTime;
+        float averageMs;
+        std::vector<std::chrono::duration<float, std::milli>> times;
+    };
 }
 
 #endif
